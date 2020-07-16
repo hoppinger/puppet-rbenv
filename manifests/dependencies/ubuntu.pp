9a918971fdb0a1cdc9d731fced582507c6b6ca5f
Rolf van de Krol <info@rolfvandekrol.nl>
2020-07-16 19:41:38 +0200

    Add libreadline clause for bionic

--- manifests/dependencies/ubuntu.pp
+++ manifests/dependencies/ubuntu.pp
@@ -9,8 +9,8 @@ class rbenv::dependencies::ubuntu {
       if ! defined(Package['libreadline5-dev']) { package { 'libreadline5-dev': ensure => installed } }
     }
     bionic: {
-      if ! defined(Package['libreadline5'])     { package { 'libreadline7':     ensure => installed } }
-      if ! defined(Package['libreadline5-dev']) { package { 'libreadline-dev': ensure => installed } }
+      if ! defined(Package['libreadline7'])     { package { 'libreadline7':     ensure => installed } }
+      if ! defined(Package['libreadline-dev']) { package { 'libreadline-dev': ensure => installed } }
     }
     default : {
       if ! defined(Package['libreadline6'])     { package { 'libreadline6':     ensure => installed } }
